112ef9d4777346bf2ec116d28ca8afc02ba1395d
Vitoriano <jessica.svitoriano@gmail.com>
2022-06-08 19:02:43 -0300

    Script JS
    Adicionado a função de mudar para cor vermelha quando o numero for negativo

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+let currentNumberWrapper = document.getElementById('currentNumber');
+let currentNumber = 0;
+let outraCor = document.getElementsByTagName('span')[0]
+
+function increment(){
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if( currentNumber > 0){
+        outraCor.style.color = 'blue'
+    }
+}
+
+function decrement(){
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+    if( currentNumber < 0 ){
+        outraCor.style.color = 'red'
+    }
+}
+
